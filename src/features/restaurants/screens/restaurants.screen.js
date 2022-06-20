493d3c77e59cc44e33bc8b91b28b54bb18560747
Agustin Ruiz <agustinomarruiz@gmail.com>
2022-06-20 12:13:05 -0300

    React Native - Meals to Go App - Creating the map feature to see the restaurants in the map, see a small detail info and navigate to the detail when you click it

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import styled from "styled-components/native";
-import { FlatList, Pressable } from "react-native";
+import { FlatList, TouchableOpacity } from "react-native";
 import { ActivityIndicator, Colors } from "react-native-paper";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
@@ -39,11 +39,15 @@ export const RestaurantsScreen = ({ navigation }) => {
         data={restaurants}
         renderItem={({ item }) => {
           return (
-            <Pressable onPress={() => navigation.navigate("RestaurantDetail")}>
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("RestaurantDetail", { restaurant: item })
+              }
+            >
               <Spacer position="bottom" size="large">
                 <RestaurantInfoCard restaurant={item} />
               </Spacer>
-            </Pressable>
+            </TouchableOpacity>
           );
         }}
         keyExtractor={(item) => item.name}
--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -1,16 +1,24 @@
 import React from "react";
-import { Text } from "react-native";
 // La navegacion por stack son ventanas apareciendo una sobre otra.
-import { createStackNavigator } from "@react-navigation/stack";
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "@react-navigation/stack";
 
 import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
+import { RestaurantDetailScreen } from "../../features/restaurants/screens/restaurant-detail.screen";
 
 const RestaurantStack = createStackNavigator();
 
 export const RestaurantsNavigator = () => {
   return (
-    <RestaurantStack.Navigator screenOptions={{ headerShown: false }}>
-      <RestaurantStack.Screen
+    <RestaurantStack.Navigator
+      screenOptions={{
+        headerShown: false,
+        ...TransitionPresets.ModalPresentationIOS,
+      }}
+    >
+      <RestaurantStack.Screen // La primer screen que aparece en el stack es la que se renderiza primero. LAs subsecuentes se renderizan como modulos
         name="RestaurantsList"
         //Al poner Restaurant Screen en el campo componente automaticamente recibe el prop navigate
         //para que dentro de restaurant screen pueda navegar a las distintas pantallas del stack.
@@ -18,7 +26,8 @@ export const RestaurantsNavigator = () => {
       />
       <RestaurantStack.Screen
         name="RestaurantDetail"
-        component={() => <Text>Restauran Detail</Text>}
+        // Los componentes tambien reciben el parametro route para traerse informacion del que los llama.
+        component={RestaurantDetailScreen}
       />
     </RestaurantStack.Navigator>
   );
